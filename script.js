9d251e8e03885f9407078d71f3972b7f5611837f
Kenny Nguyen <sokenns@gmail.com>
2023-12-14 17:45:51 -0800

    Check if user has winning combo after making move

--- script.js
+++ script.js
@@ -24,6 +24,7 @@ const computer = (function () {
 const game = (function () {
   let player_tiles = [];
   let computer_tiles = [];
+
   const getPlayerTiles = () => player_tiles;
   const getComputerTiles = () => computer_tiles;
   const makeMove = (user, num) => {
@@ -31,16 +32,48 @@ const game = (function () {
       gameBoard.fillTile(num, user.symbol);
       if (user === player) {
         player_tiles.push(num);
+        checkGame(player_tiles);
+        // Make computer do a move
       } else {
         computer_tiles.push(num);
+        checkGame(computer_tiles);
       }
     } else {
       console.log("Sorry, that spot is taken!");
     }
   };
-  const checkGame = (user) => {
-    // If user's array has winning combination of spots
-    console.log(user + " won!");
+
+  const checkGame = (tiles) => {
+    let winning_combos = [
+      [1, 2, 3],
+      [1, 5, 9],
+      [1, 4, 7],
+      [2, 5, 8],
+      [3, 5, 7],
+      [3, 6, 9],
+      [4, 5, 6],
+      [7, 8, 9],
+    ];
+
+    for (let i = 0; i < winning_combos.length; i++) {
+      let valid_combo = false;
+      let combo = winning_combos[i];
+
+      for (let j = 0; j < combo.length; j++) {
+        if (tiles.includes(combo[j])) {
+          valid_combo = true;
+        } else {
+          valid_combo = false;
+          break;
+        }
+      }
+      if (valid_combo) {
+        console.log("user has won!");
+        break;
+      } else {
+        console.log("user does not have numbers in " + winning_combos[i]);
+      }
+    }
   };
   return { getPlayerTiles, getComputerTiles, makeMove, checkGame };
 })();
