13ba56d1c7664de66efe6c00cceef4a0e157bd04
Kenny Nguyen <sokenns@gmail.com>
2023-12-15 18:10:49 -0800

    Prevent moves when game is over, add onClick funcs

--- script.js
+++ script.js
@@ -29,19 +29,23 @@ const game = (function () {
 
   const getPlayerTiles = () => player_tiles;
   const getComputerTiles = () => computer_tiles;
-  const makeMove = (user, num) => {
-    if (gameBoard.getBoard()[num - 1] === undefined) {
-      gameBoard.fillTile(num, user.symbol);
-      if (user === player) {
-        player_tiles.push(num);
-        checkGame(player.name, player_tiles);
-        // Make computer do a move
-      } else {
-        computer_tiles.push(num);
-        checkGame(computer.name, computer_tiles);
-      }
+  const makeMove = (user, num, tile) => {
+    if (game_over) {
+      console.log(game_over_message);
     } else {
-      console.log("Sorry, that spot is taken!");
+      if (gameBoard.getBoard()[num - 1] === undefined) {
+        gameBoard.fillTile(num, user.symbol);
+        if (user === player) {
+          tile.textContent = "X";
+          player_tiles.push(num);
+          checkGame(player.name, player_tiles);
+        } else {
+          computer_tiles.push(num);
+          checkGame(computer.name, computer_tiles);
+        }
+      } else {
+        console.log("Sorry, that spot is taken!");
+      }
     }
   };
 
@@ -57,39 +61,44 @@ const game = (function () {
       [7, 8, 9],
     ];
 
-    for (let i = 0; i < winning_combos.length; i++) {
-      if (game_over) {
-        console.log(game_over_message);
-        break;
-      }
-      let valid_combo = false;
-      let combo = winning_combos[i];
+    if (game_over) {
+      console.log(game_over_message);
+    } else {
+      for (let i = 0; i < winning_combos.length; i++) {
+        let valid_combo = false;
+        let combo = winning_combos[i];
 
-      for (let j = 0; j < combo.length; j++) {
-        if (tiles.includes(combo[j])) {
-          valid_combo = true;
-        } else {
-          valid_combo = false;
-          break;
+        for (let j = 0; j < combo.length; j++) {
+          if (tiles.includes(combo[j])) {
+            valid_combo = true;
+          } else {
+            valid_combo = false;
+            break;
+          }
         }
-      }
 
-      if (valid_combo) {
-        console.log(username + " has won!");
-        game_over = true;
-        game_over_message = username + " has already won, stop making moves!";
-        break;
-      } else {
-        if (!gameBoard.getBoard().includes(undefined)) {
+        if (valid_combo) {
+          console.log(username + " has won!");
+          game_over = true;
+          game_over_message = username + " has already won, stop making moves!";
+          break;
+        } else if (!gameBoard.getBoard().includes(undefined)) {
           game_over = true;
           game_over_message = "There are no more moves to make, stop trying!";
           break;
+        } else {
+          console.log(
+            username + " does not have numbers in " + winning_combos[i]
+          );
         }
-        console.log(
-          username + " does not have numbers in " + winning_combos[i]
-        );
       }
     }
   };
   return { getPlayerTiles, getComputerTiles, makeMove, checkGame };
 })();
+
+let tiles = document.querySelectorAll(".tile");
+for (let i = 0; i < tiles.length; i++) {
+  let tile = tiles[i];
+  tile.addEventListener("click", () => game.makeMove(player, i + 1, tile));
+}
